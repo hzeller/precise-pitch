41d8aa43b6ed319350be677499bdd451f52749a9
Henner Zeller <h.zeller@acm.org>
2013-08-02 22:29:43 -0700

    o Move cent-print paint out into constant.

--- src/net/zllr/precisepitch/CenterOffsetView.java
+++ src/net/zllr/precisepitch/CenterOffsetView.java
@@ -7,6 +7,12 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
+// Displays a little sequence of circles, color-filled when 'active'.
+// Something like [o o o o o O o o o o o].
+//
+// The (green) center circle represents frequency being 'in-tune', while the
+// (red) off-center circles indicate if and how far off the pitch is.
+// It attempts to mimick LEDs in a regular physical tuner.
 public class CenterOffsetView extends View {
     private static final int kWidth = 600;  // TODO: make property
     private static final int kHeight = 50;
@@ -14,6 +20,7 @@ public class CenterOffsetView extends View {
     private final Paint emptyCirclePaint;
     private final Paint filledRedCirclePaint;
     private final Paint filledGreenCirclePaint;
+    private final Paint centAnnotationPaint;
 
     private double range = 50;
     private double value;
@@ -36,33 +43,44 @@ public class CenterOffsetView extends View {
         filledGreenCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         filledGreenCirclePaint.setColor(Color.rgb(40, 255, 40));
         filledGreenCirclePaint.setStyle(Paint.Style.FILL);
+        centAnnotationPaint = new Paint();
+        centAnnotationPaint.setColor(Color.rgb(255, 255, 255));
+        centAnnotationPaint.setTextSize((int)(kHeight * 0.8));
+        centAnnotationPaint.setTextAlign(Paint.Align.CENTER);
+
         setRange(45);
         setQuantization(10);
         setValue(0);
     }
 
+    // Range being displayed. A range of 45 covers -45 .. +45.
+    // Default: 45
     public void setRange(double range) {
         if (range == this.range) return;
         this.range = range;
         invalidate();
     }
 
+    // The value to display. Values between +/- range are displayed (0 being
+    // the center-'LED'). Out of range leaves the display dark.
     public void setValue(double value) {
         if (value == this.value) return;
         this.value = value;
         invalidate();
     }
 
+    // Quantization of the range. Default: 10
     public void setQuantization(int q) {
         if (q == this.quantization) return;
-        if (q < 1) throw new IllegalArgumentException("Quantization needs to be >= 1: " + q);
+        if (q < 1) throw new IllegalArgumentException(
+                                     "Quantization needs to be >= 1: " + q);
         this.quantization = q;
         invalidate();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        setMeasuredDimension(kWidth, 2 * kHeight);  // on top we show text.
+        setMeasuredDimension(kWidth, 2 * kHeight);  // we show text below.
     }
 
     @Override
@@ -79,16 +97,13 @@ public class CenterOffsetView extends View {
                 paint = filledRedCirclePaint;
             else
                 paint = emptyCirclePaint;
-            canvas.drawCircle(kWidth/2 + i * widthSteps, kHeight/2, radius, paint);
+            canvas.drawCircle(kWidth/2 + i * widthSteps, kHeight/2,
+                              radius, paint);
         }
         if (Math.abs(value) <= range) {
-            Paint p = new Paint();
-            p.setColor(Color.rgb(255, 255, 255));
-            final int textHeight = (int) (kHeight * 0.8);
-            p.setTextSize(textHeight);
-            p.setTextAlign(Paint.Align.CENTER);
             canvas.drawText(String.format("%+.0fc", value),
-                            kWidth/2, kHeight + kHeight/2 + 2, p);
+                            kWidth/2, kHeight + kHeight/2 + 2,
+                            centAnnotationPaint);
         }
     }
 }
