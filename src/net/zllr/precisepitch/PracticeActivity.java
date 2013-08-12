4a6e9ac912f688741881eb87facb31f2a6b869c2
Henner Zeller <h.zeller@acm.org>
2013-08-11 21:29:52 -0700

    o Some more buttons for basic scales. o Toggling scales with another tap on the button.

--- src/net/zllr/precisepitch/PracticeActivity.java
+++ src/net/zllr/precisepitch/PracticeActivity.java
@@ -18,6 +18,9 @@ public class PracticeActivity extends Activity {
     private Button randomTune;
     private Button cmajor;
     private Button gmajor;
+    private Button dmajor;
+    private Button fmajor;
+    private Button bbmajor;
     private CenterOffsetView ledview;
     private Button startbutton;
     private TextView instructions;
@@ -40,7 +43,7 @@ public class PracticeActivity extends Activity {
         setContentView(R.layout.practice);
 
         staff = (StaffView) findViewById(R.id.practiceStaff);
-        staff.setNotesPerStaff(8);
+        staff.setNotesPerStaff(9);
 
         // For now we have a couple of buttons to create some basics, but
         // these should be replaced by: (a) Spinner (for choosing scales and randomTune)
@@ -53,6 +56,12 @@ public class PracticeActivity extends Activity {
         cmajor.setOnClickListener(noteCreator);
         gmajor = (Button) findViewById(R.id.newGMajor);
         gmajor.setOnClickListener(noteCreator);
+        dmajor = (Button) findViewById(R.id.newDMajor);
+        dmajor.setOnClickListener(noteCreator);
+        fmajor = (Button) findViewById(R.id.newFMajor);
+        fmajor.setOnClickListener(noteCreator);
+        bbmajor = (Button) findViewById(R.id.newBbMajor);
+        bbmajor.setOnClickListener(noteCreator);
         ledview = (CenterOffsetView) findViewById(R.id.practiceLedDisplay);
         startbutton = (Button) findViewById(R.id.practiceStartButton);
         startbutton.setOnClickListener(new View.OnClickListener() {
@@ -62,6 +71,13 @@ public class PracticeActivity extends Activity {
             }
         });
         restartbutton = (Button) findViewById(R.id.practiceRestartButton);
+        restartbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setActivityState(State.EMPTY_SCALE);
+                setActivityState(State.WAIT_FOR_START);  // We already have
+            }
+        });
         instructions = (TextView) findViewById(R.id.practiceInstructions);
 
         staff.setNoteModel(noteModel);
@@ -72,14 +88,34 @@ public class PracticeActivity extends Activity {
     // Kinda hardcoded now :)
     private final class NoteGenerationListener implements View.OnClickListener {
         public void onClick(View button) {
+            boolean wantsFlat = false;
+            int currentStartNote = 0;
+            // We use the current start note to help toggle major scales.
+            if (noteModel.size() > 0)
+                currentStartNote = noteModel.get(0).pitch;
             noteModel.clear();
             if (button == randomTune) {
+                for (int i = 0; i < 8; ++i) {
+                    noteModel.add(new StaffView.Note(
+                            3 + (int) (16 * Math.random()),
+                            4, Color.BLACK));
+                }
             } else if (button == cmajor) {
-                addMajorScale(3, true, noteModel);
+                addMajorScale(currentStartNote == 3 ? 15 : 3, true, noteModel);
             } else if (button == gmajor) {
-                addMajorScale(10, true, noteModel);
+                addMajorScale(currentStartNote == 10 ? 22 : 10, true, noteModel);
+            } else if (button == dmajor) {
+                addMajorScale(currentStartNote == 5 ? 17 : 5, true, noteModel);
+            } else if (button == fmajor) {
+                addMajorScale(currentStartNote == 8 ? 20 : 8, true, noteModel);
+                wantsFlat = true;
+            } else if (button == bbmajor) {
+                addMajorScale(currentStartNote == 13 ? 25 : 13, true, noteModel);
+                wantsFlat = true;
             }
-            staff.onModelChanged();
+
+        staff.onModelChanged();
+            staff.setKeyDisplay(wantsFlat ? 0 : 1);
             setActivityState(noteModel.size() > 0
                                      ? State.WAIT_FOR_START
                                      : State.EMPTY_SCALE);
@@ -91,38 +127,41 @@ public class PracticeActivity extends Activity {
         // do practice.
     }
 
+    private void setGeneratorButtonsVisibility(int visibility) {
+        randomTune.setVisibility(visibility);
+        gmajor.setVisibility(visibility);
+        cmajor.setVisibility(visibility);
+        dmajor.setVisibility(visibility);
+        fmajor.setVisibility(visibility);
+        bbmajor.setVisibility(visibility);
+    }
     private void setActivityState(State state) {
         switch (state) {
             case EMPTY_SCALE:
                 startbutton.setVisibility(View.INVISIBLE);
                 restartbutton.setVisibility(View.INVISIBLE);
                 ledview.setVisibility(View.INVISIBLE);
-                randomTune.setVisibility(View.VISIBLE);
-                gmajor.setVisibility(View.VISIBLE);
-                cmajor.setVisibility(View.VISIBLE);
+                setGeneratorButtonsVisibility(View.VISIBLE);
                 instructions.setText("Choose your chant of doom.");
                 break;
             case WAIT_FOR_START:
                 instructions.setText("Refine or start.");
                 startbutton.setVisibility(View.VISIBLE);
+                setGeneratorButtonsVisibility(View.VISIBLE);
                 break;
             case PRACTICE:
                 instructions.setText("Play notes. Hold when in tune.");
                 startbutton.setVisibility(View.INVISIBLE);
                 restartbutton.setVisibility(View.VISIBLE);
                 ledview.setVisibility(View.VISIBLE);
-                randomTune.setVisibility(View.INVISIBLE);
-                gmajor.setVisibility(View.INVISIBLE);
-                cmajor.setVisibility(View.INVISIBLE);
+                setGeneratorButtonsVisibility(View.INVISIBLE);
                 break;
             case FINISHED:
                 instructions.setText("Gather your prices.");
                 startbutton.setVisibility(View.INVISIBLE);
                 restartbutton.setVisibility(View.VISIBLE);
                 ledview.setVisibility(View.INVISIBLE);
-                randomTune.setVisibility(View.INVISIBLE);
-                gmajor.setVisibility(View.INVISIBLE);
-                cmajor.setVisibility(View.INVISIBLE);
+                setGeneratorButtonsVisibility(View.INVISIBLE);
         }
     }
     // Add a major scale to the model.
