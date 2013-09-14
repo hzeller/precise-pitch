611cd39368eef1c27635589fc4a8dbdd790f52ae
Oren Hazi <oren.hazi@gmail.com>
2013-09-14 15:56:29 -0700

    Basic HistogramANnotator

--- src/net/zllr/precisepitch/Histogram.java
+++ src/net/zllr/precisepitch/Histogram.java
@@ -3,11 +3,13 @@ package net.zllr.precisepitch;
 import net.zllr.precisepitch.model.MeasuredPitch;
 
 public class Histogram {
-    private static final int MAX_NOTE = 56;
-    private double histdata[][];
+    private static final int MAX_NOTE = 59;
+    private int histdata[][]; //histdata[note][cent]
+    private int maxCount[];   //maxCount[note]
     
     public Histogram() {
-        histdata = new double[MAX_NOTE][100];
+        histdata = new int[MAX_NOTE][100];
+        maxCount = new int[MAX_NOTE];
     }
     
     public Histogram(Histogram h) {
@@ -16,6 +18,7 @@ public class Histogram {
             for (int j = 0; j < h.histdata[i].length; j++) {
                 histdata[i][j] = h.histdata[i][j];
             }
+            maxCount[i] = h.maxCount[i];
         }
     }
     
@@ -24,14 +27,26 @@ public class Histogram {
             int cent = (int)(d.cent + 50.0);
             cent = Math.max(0, Math.min(cent, 99));
             histdata[d.note][cent]++;
+            if (maxCount[d.note] < histdata[d.note][cent]) {
+                maxCount[d.note] = histdata[d.note][cent];
+            }
         }
+    }
+    
+    public int count(int note, int cent) {
+        return histdata[note][cent + 50];
+    }
+    
+    public double normalized(int note, int cent) {
+        return (double)histdata[note][cent + 50] / (double)maxCount[note];
     }
     
     public void reset() {
         for (int i = 0; i < histdata.length; i++) {
             for (int j = 0; j < histdata[i].length; j++) {
-                histdata[i][j] = 0.0;
+                histdata[i][j] = 0;
             }
+            maxCount[i] = 0;
         }
     }
 }
--- src/net/zllr/precisepitch/PracticeActivity.java
+++ src/net/zllr/precisepitch/PracticeActivity.java
@@ -225,7 +225,8 @@ public class PracticeActivity extends Activity {
 
     private class PitchFollowGame extends Handler implements ProgressProvider {
         PitchFollowGame(List<DisplayNote> model) {
-            highlightAnnotator = new HighlightAnnotator(this);
+            //highlightAnnotator = new HighlightAnnotator(this);
+            highlightAnnotator = new HistogramAnnotator(new Histogram());
             running = true;
             this.model = model;
             for (DisplayNote n : model) {
@@ -338,7 +339,8 @@ public class PracticeActivity extends Activity {
 
         private final static int kHoldTime = 15;
         private final MicrophonePitchPoster pitchPoster;
-        private final HighlightAnnotator highlightAnnotator;
+        //private final HighlightAnnotator highlightAnnotator;
+        private final HistogramAnnotator highlightAnnotator;
         private final List<DisplayNote> model;
         private long startPracticeTime;
         private float sumAbsoluteOffset;
@@ -438,6 +440,102 @@ public class PracticeActivity extends Activity {
         addMajorScale(addMajorScale(startNote, true, model), false, model);
     }
 
+    private static final class HistogramAnnotator
+            implements DisplayNote.Annotator {
+        
+        private final boolean twoPlayer;
+        private final Histogram hist1;
+        private final Histogram hist2;
+        private final Paint borderPaint;
+        private final Paint histPaint;
+        
+        public HistogramAnnotator(Histogram player1, Histogram player2) {
+            twoPlayer = (player2 != null);
+            hist1 = player1;
+            hist2 = player2;
+            
+            borderPaint = new Paint();
+            borderPaint.setColor(Color.BLACK);
+            borderPaint.setStrokeWidth(0);
+            borderPaint.setStyle(Paint.Style.STROKE);
+            
+            histPaint = new Paint();
+            histPaint.setStrokeWidth(0);
+            histPaint.setAntiAlias(true);
+        }
+        
+        public HistogramAnnotator(Histogram player1) {
+            this(player1, null);
+        }
+        
+        private static int getJetColor(double value) {
+            //value from 0.0 to 1.0
+            int fourValue = (int)(4.0 * 256.0 * value);
+            int red   = Math.min(fourValue - 384, -fourValue + 1152);
+            int green = Math.min(fourValue - 128, -fourValue + 896);
+            int blue  = Math.min(fourValue + 128, -fourValue + 640);
+            red   = Math.min(255, Math.max(0, red));
+            green = Math.min(255, Math.max(0, green));
+            blue  = Math.min(255, Math.max(0, blue));
+            
+            return Color.rgb(red, green, blue);
+        }
+        
+        public void draw(DisplayNote note, Canvas canvas,
+                         RectF staffBoundingBox, RectF noteBoundingBox) {
+            
+            float halfWidth = (noteBoundingBox.right - noteBoundingBox.left) / 2;
+            float centerX = noteBoundingBox.left + halfWidth;
+            
+            float histTop = .5f * halfWidth;
+            float histBottom = staffBoundingBox.top - (1.2f * halfWidth);
+            
+            if (noteBoundingBox.top < histBottom) {
+                //squeeze histogram box above note
+                histBottom = noteBoundingBox.top - (0.2f * halfWidth);
+                
+                //if we're squeezing too much, move histogram box to bottom
+                float boxTopHeight = histBottom - histTop;
+                float bottomBoxTop = staffBoundingBox.bottom + (0.2f * halfWidth);
+                float bottomBoxBottom = canvas.getHeight() - (0.2f * halfWidth);
+                if (boxTopHeight < (bottomBoxBottom - bottomBoxTop)) {
+                    histTop = bottomBoxTop;
+                    histBottom = bottomBoxBottom;
+                }
+            }
+            
+            if (twoPlayer) {
+                RectF histBox1 = new RectF(centerX - halfWidth, histTop,
+                                     centerX, histBottom);
+                RectF histBox2 = new RectF(centerX, histTop,
+                                     centerX + halfWidth, histBottom);
+                for (int i = 0; i < 100; i++) {
+                    //fill in histograms
+                }
+                canvas.drawRect(histBox1, borderPaint);
+                canvas.drawRect(histBox2, borderPaint);
+            } else {
+                RectF histBox = new RectF(centerX - halfWidth, histTop,
+                                     centerX + halfWidth, histBottom);
+                //fill in histogram
+                RectF histSlice = new RectF(histBox);
+                for (int i = 0; i < 100; i++) {
+                    histSlice.bottom = histBox.bottom - (i*histBox.height()/100.0f);
+                    histSlice.top = histBox.bottom - ((i+1)*histBox.height()/100.0f);
+                    
+                    histPaint.setColor(getJetColor((double)i/100.0));
+                    //histPaint.setColor(Color.RED);
+                    canvas.drawRect(histSlice, histPaint);
+                    System.out.println("Drawing slice: " + i +
+                        "(bot: " + histSlice.bottom + " top: " + histSlice.top + ")"
+                    );
+                }
+                canvas.drawRect(histBox, borderPaint);
+            }
+            
+        }
+    }
+    
     private static final class HighlightAnnotator
             implements DisplayNote.Annotator {
         private final Paint highlightPaint;
