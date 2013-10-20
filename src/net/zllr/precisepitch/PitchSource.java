2dabacafa664b656fa6e3acaddb83f2b3d54657e
Henner Zeller <h.zeller@acm.org>
2013-10-20 14:01:49 -0700

    o smallish comment updates and remove of unneded imports.

--- src/net/zllr/precisepitch/PitchSource.java
+++ src/net/zllr/precisepitch/PitchSource.java
@@ -1,10 +1,25 @@
+/*
+ * Copyright 2013 Henner Zeller <h.zeller@acm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package net.zllr.precisepitch;
 
 import android.os.Handler;
-import net.zllr.precisepitch.model.MeasuredPitch;
 
-// A PitchSource runs a thread to asynchronously gather pitch information. This
-// could be for instance listening on a microphone.
+// A PitchSource runs a thread to asynchronously gather pitch information and
+// sends it to the message handler queue to process.
 public interface PitchSource {
     // Set handler for messages generated from this PitchSource.
     // This sends 'MeasuredPitch' messages.
